c293559e0c16934371e65912131a39614e12578a
loremanu <lorenrr1@gmail.com>
2016-02-06 20:42:53 +0100

    Añadiendo modificacion settings

--- MiTienda/settings.py
+++ MiTienda/settings.py
@@ -124,6 +124,14 @@ INSTALLED_APPS = (
 )
 
 
+import django
+
+if django.VERSION < (1, 7):
+    INSTALLED_APPS += (
+        'south',
+    )
+
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
